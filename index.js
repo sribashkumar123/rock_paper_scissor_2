9a23c2fc517118bb074ad1d54fb3a08f99053720
Sribash kumar das <87633338+sribashkumar123@users.noreply.github.com>
2023-10-10 13:02:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,197 +1,214 @@
-
-const rulebtn = document.querySelectorAll(".rules-btn");
-const nextbtn=document.getElementById("next-btn");
-const playAgainbtn = document.querySelector("#play-again");
-const replaybtn = document.querySelector("#replay");
-const closeModalbtn = document.getElementById("close");
-const rulesModal = document.getElementById("rules-modal");
-const wonGame = document.querySelector(".won-game");
-const playBoard = document.getElementById("play-board");
-const resultBoard = document.getElementById("result-board");
-const userResult = document.querySelector(".user-result");
-const pcResult = document.querySelector(".pc-result");
-let resultText = document.getElementById("result-text-1");
-let resultText2 = document.getElementById("result-text-2");
-let picked = document.querySelectorAll(".picked");
-const computerScore = document.getElementById("computer-score");
-const userScore = document.getElementById("user-score");
-
-
-let score = {
-  user: 0,
-  computer: 0,
-};
-
-
-if (localStorage.getItem("score")) {
-  score = JSON.parse(localStorage.getItem("score"));
-}
-
-userScore.innerHTML = score.user;
-computerScore.innerHTML = score.computer;
-
-
-const result = {
-  WIN: "YOU WIN",
-  LOST: "YOU LOST",
-  TIEUP: "TIE UP",
-};
+const close_btn=document.getElementById("close");
+const rules=document.getElementById("all_rules");
+const rules_btn=document.getElementById("rules_btn");
+const rock_btn=document.getElementsByClassName("rock");
+const paper_btn=document.getElementsByClassName("paper");
+const sessior_btn=document.getElementsByClassName("sessior");
+const user_result_action=document.getElementsByClassName("user_result_action");
+const pc_result_action=document.getElementsByClassName("pc_result_action");
+const result_action=document.querySelector(".result_action");
+const result_winner=document.querySelector(".result_winner");
+const result_box=document.querySelector(".result_box");
+const playing_area= document.querySelector(".playing_area");
+const play_again_btn=document.querySelector(".play_again_btn");
+const next_btn=document.getElementById("next_btn");
+const user_score=document.getElementById("user_score");
+const pc_score=document.getElementById("pc_score");
+const userwin_box1=document.getElementById("userwinner_box1");
+const userwin_box2=document.getElementById("userwinner_box2");
+const pcwin_box1=document.getElementById("pcwinner_box1");
+const pcwin_box2=document.getElementById("pcwinner_box2");
+const conatinercls=document.querySelector(".container");
+const hurraypage=document.querySelector(".hurraypage");
+const hurray_rule_btn=document.getElementById("hurray_rule_btn");
 
 
 
+next_btn.addEventListener("click",()=>
+{
+    conatinercls.style.display="none";
+    hurraypage.style.display="flex";
 
-rulebtn.forEach((element) => {
-  element.addEventListener("click", () => {
-    rulesModal.style.display = "block";
-  });
+})
+
+
+let game_score = { user: 0, computer: 0};
+if (localStorage.getItem("game_score")) {
+    game_score = JSON.parse(localStorage.getItem("game_score"));
+  }
+  
+  user_score.innerHTML = game_score.user;
+  pc_score.innerHTML = game_score.computer;
+
+  // localStorage.setItem("score", JSON.stringify(score));
+
+
+
+
+
+
+
+
+
+let user_click_btn="";
+close_btn.addEventListener("click",()=>
+{
+    rules.style.display="none";
 });
 
-closeModalbtn.addEventListener("click", () => {
-  rulesModal.style.display = "none";
+rules_btn.addEventListener("click",()=>
+{
+    rules.style.display="block";
+});
+
+rock_btn[0].addEventListener("click",()=>
+{
+    user_click_btn="rock";
+    result_box.style.display="flex";
+    playing_area.style.display="none";
+    changeimage(user_click_btn);
+    changeimage_computer();
+    game_deccsion();
+});
+
+paper_btn[0].addEventListener("click",()=>
+{
+    user_click_btn="paper";
+    result_box.style.display="flex";
+    playing_area.style.display="none";
+    changeimage(user_click_btn);
+    changeimage_computer();
+    game_deccsion();
+    
 });
 
 
+sessior_btn[0].addEventListener("click",()=>
+{
+    user_click_btn="scissor";
+    result_box.style.display="flex";
+    playing_area.style.display="none";
+    changeimage(user_click_btn);
+    changeimage_computer();
+    game_deccsion();
+    
+});
 
-playAgainbtn.addEventListener("click", playAgain);
 
-replaybtn.addEventListener("click", playAgain);
-
-function playAgain() {
-  playBoard.style.display = "grid";
-  resultBoard.style.display = "none";
-  // wonGame.style.display = "none";
-  nextbtn.style.display = "none";
-}
 const computer = ["rock", "paper", "scissor"];
 function computerPicked() {
   let picked = Math.floor(Math.random() * computer.length);
-  console.log(picked);
   return computer[picked];
 }
 
-function setImg(picked) {
-  let img = `<img src="./images/${picked}.png" alt=${picked} width="60px"/>`;
-  return img;
-}
-
-function setStyles() {
-
-  resultBoard.style.marginTop = "3rem";
-
-  picked.forEach((element) => {
-    element.style.top = "300px";
-  });
-
-  for (let index = 0; index < 3; index++) {
-    userResult.classList.remove("rock-div");
-    userResult.classList.remove("paper-div");
-    userResult.classList.remove("scissor-div");
-    pcResult.classList.remove("rock-div");
-    pcResult.classList.remove("paper-div");
-    pcResult.classList.remove("scissor-div");
-
-    playAgainbtn.style.display = "block";
-    resultText2.style.display = "block";
-    replaybtn.style.display = "none";
-    nextbtn.style.display = "none";
+function set_images(picked) {
+    let img = `<img src="./images/${picked}.png" alt=${picked} width="60px"/>`;
+    return img;
   }
-}
-
-const startGame = (userPicked) => {
-
-  let pcPicked = computerPicked();
-
-  setStyles();
-
-  let res;
-
-  if (userPicked === pcPicked) {
-
-    res = result.TIEUP;
-
-    removeFocus();
-
-    playAgainbtn.style.display = "none";
-    replaybtn.style.display = "block";
-    resultText2.style.display = "none";
-
-    picked.forEach((element) => {
-      element.style.top = "256px";
-    });
-
-    resultBoard.style.marginTop = "6rem";
-
-  } 
-  else if (
-    (userPicked === "rock" && pcPicked === "scissor") ||
-    (userPicked === "paper" && pcPicked === "rock") ||
-    (userPicked === "scissor" && pcPicked === "paper")
-  ) {
-    res = result.WIN;
-
-    nextbtn.style.display = "block";
-
-    focusOnUserWinner();
-    score.user++;
-
-  } 
-  else {
-    res = result.LOST;
-    focusOnPCWinner();
-    score.computer++;
-
-  }
-  playBoard.style.display = "none";
-  resultBoard.style.display = "flex";
-
- 
-  userResult.classList.add(`${userPicked}-div`);
-  pcResult.classList.add(`${pcPicked}-div`);
-  userResult.innerHTML = setImg(userPicked);
-  pcResult.innerHTML = setImg(pcPicked);
-  resultText.innerHTML = res;
-
-
-  userScore.innerHTML = score.user;
-  computerScore.innerHTML = score.computer;
-
-  localStorage.setItem("score", JSON.stringify(score));
-};
-
-
-
-let winUserBox1 = document.querySelector(".user-box-1");
-let winUserBox2 = document.querySelector(".user-box-2");
-let winUserBox3 = document.querySelector(".user-box-3");
-let winPcBox1 = document.querySelector(".pc-box-1");
-let winPcBox2 = document.querySelector(".pc-box-2");
-let winPcBox3 = document.querySelector(".pc-box-3");
-
-let focusOnUserWinner = () => {
-  winPcBox1.classList.remove("winner-box-1");
-  winPcBox2.classList.remove("winner-box-2");
   
+  function changeimage_computer()
+  {
+  for (let i = 0; i < pc_result_action.length; i++) {
+    pc_result_action[i].innerHTML = set_images(computerPicked());
+  }
+}
 
-  winUserBox1.classList.add("winner-box-1");
-  winUserBox2.classList.add("winner-box-2");
+function changeimage(userchoice)
+{
+  for (let i = 0; i < user_result_action.length; i++) {
+    user_result_action[i].innerHTML = set_images(userchoice);
+  }
+}
+function game_deccsion()
+{
+    console.log(user_click_btn);
+    const c_pick=computerPicked();
+    console.log(c_pick);
+    
+    
+    if (user_click_btn === c_pick) {
+      console.log("Match Tie");
+      result_winner.innerHTML="MatcH Tie";
+      pcwin_box1.style.background="transparent";
+      pcwin_box2.style.background="transparent";
+      userwin_box1.style.background="transparent";
+        userwin_box2.style.background="transparent";
+    } 
+    else if (
+      (user_click_btn === "rock" && c_pick === "scissor") ||
+      (user_click_btn === "paper" && c_pick === "rock") ||
+      (user_click_btn === "scissor" && c_pick === "paper")
+    ) {
+        game_score.user++;
+        userwin_box1.style.background="#66b248";
+        userwin_box2.style.background="#32a62f";
+        pcwin_box1.style.background="transparent";
+      pcwin_box2.style.background="transparent";
+      
+        console.log(game_score.user);
+        updatescore();
+        localStorage.setItem("game_score", JSON.stringify(game_score));
 
-};
-let focusOnPCWinner = () => {
-  winUserBox1.classList.remove("winner-box-1");
-  winUserBox2.classList.remove("winner-box-2");
- 
+      console.log("user win");
+      next_btn.style.display="block";
+      result_winner.innerHTML="YOU WIN AGAINST PC";
+      
+      play_again_btn.innerHTML="REPLAY";
+      
+    } else {
+        game_score.computer++;
+        pcwin_box1.style.background="#66b248";
+        pcwin_box2.style.background="#32a62f";
+        userwin_box1.style.background="transparent";
+        userwin_box2.style.background="transparent";
+        localStorage.setItem("game_score", JSON.stringify(game_score));
+        updatescore();
+        result_winner.innerHTML="YOU LOSE AGAINST PC";
+        console.log(game_score.computer);
+        
+        
+        console.log("pc win");
+     
+    }  
 
-  winPcBox1.classList.add("winner-box-1");
-  winPcBox2.classList.add("winner-box-2");
- 
-};
+    
+}
 
-let removeFocus = () => {
-  winUserBox1.classList.remove("winner-box-1");
-  winUserBox2.classList.remove("winner-box-2");
+function updatescore()
+{
+user_score.innerHTML = game_score.user;
+pc_score.innerHTML = game_score.computer;
+
+}
+
+// ////////////////////////////////plya button////////////////////////////////////////////////////
 
 
-  winPcBox1.classList.remove("winner-box-1");
-  winPcBox2.classList.remove("winner-box-2");
- 
-};
+play_again_btn.addEventListener("click",()=>
+{
+    result_box.style.display="none";
+    playing_area.style.display="flex";
+    next_btn.style.display="none";
+}
+);
+
+/////////////////////////////////////////json score///////////////////////////
+
+
+const hurray_all_rules=document.getElementById("hurray_all_rules");
+const hurray_close=document.getElementById("hurray_close");
+
+//////////////////////////////////hurraypage///////////////////////////
+hurray_rule_btn.addEventListener("click",()=>
+{
+  hurray_all_rules.style.display="block";
+  console.log("hurray ruke btn");
+})
+
+hurray_close.addEventListener("click",()=>
+{
+  hurray_all_rules.style.display="none";
+})
+
+
+
